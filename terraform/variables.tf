9c1c42743514c0935f145e6bf5cf8855026d4513
Joshuel Nobre <nobrej1999@gmail.com>
2024-09-28 19:29:28 -0300

    terraform ecs app

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "region" {}
+
+variable "cluster_name" {}
+
+variable "service_name" {}
+
+variable "service_port" {}
+
+variable "service_cpu" {}
+
+variable "service_memory" {}
+
+variable "service_listener" {}
+
+variable "ssm_vpc_id" {}
+
+variable "ssm_listener" {}
+
+variable "ssm_private_subnet_1" {}
+
+variable "ssm_private_subnet_2" {}
+
+variable "ssm_private_subnet_3" {}
+
+variable "environment_variables" {}
+
+variable "capabilities" {}
